cfc6ccb23f1941563323ecc513271219882a0bbf
Rahul Sain <sainrahul374@gmail.com>
2026-08-19 21:43:37 +0530

    chore(backend): define the maxUploadSize for image upload in config and convert it into integer

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,23 +1,34 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+	"strconv"
+)
 
 
 type Config struct{
 	PORT string
-	MaxUploadSize string
+	MaxUploadSize int
+	FrontendURL string
 }
 
 func Load()Config{
+	maxUploadSize,err := strconv.Atoi(getEnv("MAX_UPLOAD_SIZE","10485760"))
+	if err != nil {
+		log.Fatal("error parsing MAX_UPLOAD_SIZE:", err)
+	}
+
+
 	return Config{
 		PORT: getEnv("PORT","3000"),
-		MaxUploadSize: getEnv("MAX_UPLOAD_SIZE","10485760"),
+		MaxUploadSize: maxUploadSize,
+		FrontendURL: getEnv("FRONTEND_URL","http://localhost:5173"),
 	}
 }
 
 func getEnv(key string, fallback string)string{
 	value:=os.Getenv(key)
-
 	if value==""{
 		return fallback
 	}
